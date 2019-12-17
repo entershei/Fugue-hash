da8e8b1a5d43dbbc4af11d91e0f7397f793063d4
Irina <entershei@gmail.com>
2019-12-18 00:38:32 +0300

    Add super_mixed().

--- main.cpp
+++ main.cpp
@@ -55,8 +55,92 @@ void cmix() {
     s[17] += s[6];
 }
 
-void smix() {
+uint8_t get_byte(uint32_t number, int index) { // index = 0, 1, 2, 3
+    uint32_t x = number << (8 * index);
+    return uint8_t(x >> 24);
+}
 
+vector<vector<uint8_t >> M = {{1, 4, 7, 1},
+                              {1, 1, 4, 7},
+                              {7, 1, 1, 4},
+                              {4, 7, 1, 1}};
+
+vector<vector<uint8_t >> M_t = {{1, 1, 7, 4},
+                                {4, 1, 1, 7},
+                                {7, 4, 1, 1},
+                                {1, 7, 4, 1}};
+
+vector<vector<uint8_t >> multiply_matrix_4(vector<vector<uint8_t >> const &a, vector<vector<uint8_t >> const &b) {
+    vector<vector<uint8_t >> result(4, vector<uint8_t>(4, 0));
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            for (int k = 0; k < 4; ++k)
+                result[i][j] += (a[i][k] * b[k][j]);
+        }
+    }
+    return result;
+}
+
+vector<vector<uint8_t >> sum_matrix_4(vector<vector<uint8_t >> const &a, vector<vector<uint8_t >> const &b) {
+    vector<vector<uint8_t >> result(4, vector<uint8_t>(4, 0));
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            result[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    return result;
+}
+
+vector<vector<uint8_t >> get_u_matrix() {
+    vector<vector<uint8_t >> result(4, vector<uint8_t>(4, 0));
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            result[i][j] = get_byte(s[j], i);
+        }
+    }
+    return result;
+}
+
+vector<vector<uint8_t >> get_sum_u_matrix(vector<vector<uint8_t >> const &u) {
+    vector<vector<uint8_t >> result(4, vector<uint8_t>(4, 0));
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            if (i == j) {
+                continue;
+            }
+            result[i][i] += u[i][j];
+        }
+    }
+    return result;
+}
+
+vector<vector<uint8_t >> rol_matrix(vector<vector<uint8_t >> const &w) {
+    vector<vector<uint8_t >> result(4, vector<uint8_t>(4, 0));
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            result[i][j] = w[i][(j + i) % 4];
+        }
+    }
+    return result;
+}
+
+void set_rol_w_in_s(vector<vector<uint8_t >> const &w) {
+    s[0] = s[1] = s[2] = s[3] = 0;
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            s[j] += (w[i][j] << (24 - i * 8));
+        }
+    }
+}
+
+void super_mixed() {
+    vector<vector<uint8_t >> u = get_u_matrix();
+    vector<vector<uint8_t >> m_u = multiply_matrix_4(M, u);
+    vector<vector<uint8_t >> sum_u = get_sum_u_matrix(u);
+    vector<vector<uint8_t >> sum_u_m_t = multiply_matrix_4(sum_u, M_t);
+    vector<vector<uint8_t >> w = sum_matrix_4(m_u, sum_u_m_t);
+    vector<vector<uint8_t >> rol_w = rol_matrix(w);
+    set_rol_w_in_s(rol_w);
 }
 
 vector<uint32_t> fugue_hash(string message) {
@@ -74,26 +158,26 @@ vector<uint32_t> fugue_hash(string message) {
         for (int j = 0; j < 2; ++j) {
             ror(3);
             cmix();
-            smix();
+            super_mixed();
         }
     }
 
     for (int i = 0; i < 10; ++i) {
         ror(3);
         cmix();
-        smix();
+        super_mixed();
     }
 
     for (int i = 0; i < 13; ++i) {
         s[4] += s[0];
         s[15] += s[0];
         ror(15);
-        smix();
+        super_mixed();
 
         s[4] += s[0];
         s[16] += s[0];
         ror(14);
-        smix();
+        super_mixed();
     }
 
     s[4] += s[0];
@@ -103,13 +187,16 @@ vector<uint32_t> fugue_hash(string message) {
 }
 
 int main() {
-    string s;
-    cin >> s;
+    cout << "Enter your message to hashing:" << endl;
 
-    vector<uint32_t> hash_ = fugue_hash(s);
+    string message;
+    cin >> message;
 
-    for (int i = 0; i < hash_.size(); ++i) {
-        cout << hash_[i] << " ";
+    vector<uint32_t> hash_ = fugue_hash(message);
+
+    cout << "Hash:" << endl;
+    for (unsigned int i : hash_) {
+        cout << i << " ";
     }
     cout << endl;
 
